750709b1e0c12c575e6aadec26245e9cca35d247
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-11 02:47:09 -0600

    Update salesforce.py

--- functions/salesforce.py
+++ functions/salesforce.py
@@ -10,3 +10,22 @@ def connect_to_salesforce():
         security_token=st.secrets.salesforce.sfToken
     )
     return sf
+
+# Function to execute the Salesforce query and return a DataFrame
+def fetch_cases(sf):
+    query = """
+    SELECT Id, AccountId, Account.Name, Account.ShippingStreet, Account.ShippingCity,
+    Account.ShippingState, Account.ShippingPostalCode, Account.ShippingLongitude,
+    Account.ShippingLatitude FROM Case Where Account.Subsidiary__c = 'Alma Lasers , Inc.' Limit 200
+    """
+    data = sf.query(query)
+    records = data['records']
+    data_new = []
+    for record in records:
+        
+    df = pd.DataFrame(query_result['records']).drop(columns='attributes')
+    return df
+
+# Function to update a case in Salesforce
+def update_case(sf, case_id, update_fields):
+    sf.Case.update(case_id, update_fields)
