0da9df21ff401cf18ce25f4c77f774a4369abcd5
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-11 12:21:39 -0600

    Update 3_Case.py

--- pages/3_Case.py
+++ pages/3_Case.py
@@ -9,9 +9,11 @@ import streamlit_modal as modal
 def fetch_cases():
     sf = Salesforce(username=st.secrets.salesforce.sfUsername, password=st.secrets.salesforce.sfPassword, security_token=st.secrets.salesforce.sfToken)
     query = """
-        SELECT Id, AccountId, Account.Name, Account.ShippingStreet, Account.ShippingCity,
+        SELECT Id, Status, Type, Queues__c, Owner.Name AccountId, Account.Name, Account.ShippingStreet, Account.ShippingCity,
         Account.ShippingState, Account.ShippingPostalCode, Account.ShippingLongitude,
-        Account.ShippingLatitude FROM Case Where Account.Subsidiary__c = 'Alma Lasers , Inc.' Limit 200
+        Account.ShippingLatitude FROM Case Where Account.Subsidiary__c = 'Alma Lasers , Inc.' AND Status = 'In Process' AND Type = 'Technical Support' AND Queues__c = 'Client Relations'
+        ORDER BY Created_Date_Custom__c DESC
+        Limit 200
         """
     data = sf.query(query)
     records = data['records']
@@ -21,6 +23,9 @@ def fetch_cases():
             'caseid': record['Id'],
             'accountid': record['AccountId'],
             'accountname': record.get('Account', {}).get('Name', ''),
+            'status': record['Status'],
+            'type': record['Type'],
+            'queue': record['Queues__c],
             # Add other fields as needed
         }
         data_new.append(row_data)
@@ -40,6 +45,9 @@ def show_case_modal(case):
                 st.text(f"Case ID: {case['caseid']}")
                 st.text(f"Account ID: {case['accountid']}")
                 st.text(f"Account Name: {case['accountname']}")
+                st.text(f"Status: {case['status']}")
+                st.text(f"Type: {case['type']}")
+                st.text(f"Queue: {case['queue']}")
                 # Add other case details as needed
 
             with col2:
@@ -59,11 +67,14 @@ def show_case_modal(case):
 
 def generate_row(row_id, case):
     row_container = st.empty()
-    row_columns = row_container.columns((2, 2, 1))
+    row_columns = row_container.columns((2, 2, 2, 2, 2, 1))
     row_columns[0].write(case['accountname'])
     row_columns[1].write(case['accountid'])
+    row_columns[2].write(case['status'])
+    row_columns[3].write(case['type'])
+    row_columns[4].write(case['queue'])
 
-    if row_columns[2].button("Details", key=f"btn_{row_id}"):
+    if row_columns[5].button("Details", key=f"btn_{row_id}"):
         show_case_modal(case)
 
 def main():
