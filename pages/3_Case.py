f6b49677aa649677085f127cd3b8f74ea01ca83d
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-11 12:02:19 -0600

    Update 3_Case.py

--- pages/3_Case.py
+++ pages/3_Case.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from simple_salesforce import Salesforce
 import uuid
-from streamlit_modal import Modal
+import streamlit_modal as modal
 
 # Function to fetch cases from Salesforce
 @st.cache_data
