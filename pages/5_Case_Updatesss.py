8f87964db71de542d03ef462128d08e4a7ea0bb3
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-11 09:56:03 -0600

    Update 5_Case_Updatesss.py

--- pages/5_Case_Updatesss.py
+++ pages/5_Case_Updatesss.py
@@ -4,68 +4,53 @@ import pandas as pd
 from simple_salesforce import Salesforce
 from functions import pagesetup as ps
 
-
-
 # Fetch Data
-@st.cache_data
+@st.cache
 def fetch_cases():
-  sf = Salesforce(username=st.secrets.salesforce.sfUsername, password=st.secrets.salesforce.sfPassword, security_token=st.secrets.salesforce.sfToken)
-  query = """
-    SELECT Id, AccountId, Account.Name, Account.ShippingStreet, Account.ShippingCity,
-    Account.ShippingState, Account.ShippingPostalCode, Account.ShippingLongitude,
-    Account.ShippingLatitude FROM Case Where Account.Subsidiary__c = 'Alma Lasers , Inc.' Limit 200
-    """
-  data = sf.query(query)
-  records = data['records']
-  data_new = []
-  for record in records:
-    row_data = {
-            'caseid': record['Id'],
-            'accountid': record['AccountId'],
-            'accountname': record['Account']['Name'],
-            'street': record['Account']['ShippingStreet'],
-            'city': record['Account']['ShippingCity'],
-            'state': record['Account']['ShippingState'],
-            'zipcode': record['Account']['ShippingPostalCode'],
-            'longitude': record['Account']['ShippingLongitude'],
-            'latitude': record['Account']['ShippingLatitude']
-        }
-    data_new.append(row_data)
-  df = pd.DataFrame(data_new)
-  return df
-
-# Function to update a case in Salesforce
-def update_case(sf, case_id, update_fields):
-    sf.Case.update(case_id, update_fields)
-
-
-# Function to create modal
-def create_modal(title, key, padding, width):
-  new_modal = Modal(
-    title=title,
-    key=key,
-    padding=20,
-    max_width=744
-  )
-  return new_modal
+    sf = Salesforce(username=st.secrets.salesforce.sfUsername, password=st.secrets.salesforce.sfPassword, security_token=st.secrets.salesforce.sfToken)
+    query = """
+        SELECT Id, AccountId, Account.Name, Account.ShippingStreet, Account.ShippingCity,
+        Account.ShippingState, Account.ShippingPostalCode, Account.ShippingLongitude,
+        Account.ShippingLatitude FROM Case Where Account.Subsidiary__c = 'Alma Lasers , Inc.' Limit 200
+        """
+    data = sf.query(query)
+    records = data['records']
+    data_new = []
+    for record in records:
+        # Ensure 'Id' is present in the record
+        if 'Id' in record:
+            row_data = {
+                'caseid': record['Id'],
+                'accountid': record['AccountId'],
+                'accountname': record.get('Account', {}).get('Name', ''),
+                'street': record.get('Account', {}).get('ShippingStreet', ''),
+                'city': record.get('Account', {}).get('ShippingCity', ''),
+                'state': record.get('Account', {}).get('ShippingState', ''),
+                'zipcode': record.get('Account', {}).get('ShippingPostalCode', ''),
+                'longitude': record.get('Account', {}).get('ShippingLongitude', ''),
+                'latitude': record.get('Account', {}).get('ShippingLatitude', '')
+            }
+            data_new.append(row_data)
+    return pd.DataFrame(data_new)
 
 def main():
-  #Set Title
-  ps.set_title("Field Service", "Case Updates")
+    # Set Title
+    ps.set_title("Field Service", "Case Updates")
 
-  dfCase = fetch_cases()
-  containerdf = st.container()
-  with containerdf:
-    edited_df = st.data_editor(dfCase, key="casedf_edited", num_rows = "dynamic")
+    df = fetch_cases()
+    containerdf = st.container()
+    with containerdf:
+        # Use a conditional check to ensure 'Id' is a column in the DataFrame
+        if 'Id' in df.columns:
+            edited_df = st.data_editor(df, key="casedf_edited", num_rows="dynamic")
+        else:
+            st.error("Error: 'Id' column is missing in the data")
 
-containerdf2 = st.container()
-with containerdf2:
-  exp = st.expander("View changes made", expanded = False)
-  with exp:
-    st.write(st.session_state.casedf_edited)
-    
-  
-  
-  
+    containerdf2 = st.container()
+    with containerdf2:
+        exp = st.expander("View changes made", expanded=False)
+        with exp:
+            st.write(st.session_state.get('casedf_edited', 'No changes made'))
+
 if __name__ == "__main__":
     main()
